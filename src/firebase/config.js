b9ade65c220e598e803688d9dde38bc4375c77b6
Akhila Vijayan <akhilavijayan964@gmail.com>
2022-08-08 23:35:23 +0530

    firebase storage

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBwO79OnnZV2CzWKzdNWvS7TrxFPHUf-Wk",
+    authDomain: "olx-clone-115ec.firebaseapp.com",
+    projectId: "olx-clone-115ec",
+    storageBucket: "olx-clone-115ec.appspot.com",
+    messagingSenderId: "624050048766",
+    appId: "1:624050048766:web:9a956a4e63fc79064c9faf",
+    measurementId: "G-H8Z697C6QT"
+  };
+
+
+export const Firebase= firebase.initializeApp(firebaseConfig)
