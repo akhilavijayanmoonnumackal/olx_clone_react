8e94f2f5abf737a6741c37ceb9ac42e09c3a69f9
Akhila Vijayan <akhilavijayan964@gmail.com>
2022-08-09 13:57:12 +0530

    signup completed

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -2,9 +2,11 @@ import React, { useState, useContext } from 'react';
 
 import Logo from '../../olx-logo.png';
 import { FirebaseContext } from '../../store/FirebaseContext';
+import { useHistory } from 'react-router-dom';
 import './Signup.css';
 
 export default function Signup() {
+  const history=useHistory();
   const [username,setUsername]=useState('');
   const [email,setEmail]=useState('');
   const [phone,setPhone]=useState('');
@@ -14,7 +16,15 @@ export default function Signup() {
   const handleSubmit =(e)=>{
     e.preventDefault()
     firebase.auth().createUserWithEmailAndPassword(email,password).then((result)=>{
-      result.user.updateProfile({displayName:username})
+      result.user.updateProfile({displayName:username}).then(()=>{
+        firebase.firestore().collection('users').add({
+          id:result.user.uid,
+          username:username,
+          phone:phone
+        }).then(()=>{
+            history.push('/login')
+        })
+      })
     })
   }
   return (
